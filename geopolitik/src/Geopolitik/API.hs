3f6458ff6567d221d7c21379559e13aa2bb572b0
Samuel Schlesinger <samuel@simspace.com>
2019-12-07 02:57:51 -0500

    fixed some stuff

--- geopolitik/src/Geopolitik/API.hs
+++ geopolitik/src/Geopolitik/API.hs
@@ -1,35 +1,37 @@
 {-# LANGUAGE DeriveAnyClass #-}
+
 module Geopolitik.API where
 
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+import Data.Void
+import GHC.Generics (Generic)
+import Geopolitik.Ontology
+import qualified Network.Wai as Wai
 import Servant
 import Servant.Server.Experimental.Auth
-import Data.Text (Text)
-import Geopolitik.Ontology
-import Data.Void
-import Data.Time.Clock (UTCTime)
-import qualified Network.Wai as Wai
-import Data.Aeson (ToJSON, FromJSON)
-import GHC.Generics (Generic)
 import Web.Cookie
 
-type GeopolitikAPI = "account" :> AccountAPI 
-                :<|> "article" :> ArticleAPI
-                :<|> Raw
+type GeopolitikAPI
+     = "account" :> AccountAPI 
+  :<|> "article" :> ArticleAPI 
+  :<|> Raw
 
-type AccountAPI = "signup" :> P SignUp
-             :<|> "signin" :> C SignIn
-             :<|> AuthProtect "geopolitik-user"
-                  :> ( "new-token" :> S (Response SignIn) )
+type AccountAPI
+     = "signup" :> P SignUp 
+  :<|> "signin" :> C SignIn 
+  :<|> AuthProtect "geopolitik-user" :> ("new-token" :> S (Response SignIn))
 
-type ArticleAPI = AuthProtect "geopolitik-user" :> 
-                ( "new"    :> P NewArticle
-             :<|> "draft"  :> DraftAPI ) 
-             
-type DraftAPI = "new"      :> P NewDraft
-           :<|> "comments" :> G "draft-key" DraftComments
-           :<|> "link"   :> P LinkDraft
-           :<|> "latest" :> G "article-key" LatestDraft 
-           :<|> Capture "username" Text :> Capture "article-name" Text :> Get '[JSON] (Response LatestDraft) 
+type ArticleAPI
+   = AuthProtect "geopolitik-user" :> ("new" :> P NewArticle :<|> "draft" :> DraftAPI)
+
+type DraftAPI
+     = "new" :> P NewDraft 
+  :<|> "comments" :> G "draft-key" DraftComments 
+  :<|> "link" :> P LinkDraft 
+  :<|> "latest" :> G "article-key" LatestDraft 
+  :<|> Capture "username" Text :> Capture "article-name" Text :> Get '[ JSON] (Response LatestDraft)
 
 type Ctx = AuthHandler Wai.Request User ': '[]
 
@@ -40,80 +42,98 @@ type family Request b where
 
 type instance AuthServerData (AuthProtect "geopolitik-user") = User
 
-type P req = ReqBody '[JSON] req 
-          :> Post '[JSON] (Response req)
+type P req = ReqBody '[ JSON] req :> Post '[ JSON] (Response req)
 
-type G t req = Capture t req 
-            :> Get '[JSON] (Response req) 
+type G t req = Capture t req :> Get '[ JSON] (Response req)
 
-type C req = ReqBody '[JSON] req 
-          :> S (Response SignIn)
+type C req = ReqBody '[ JSON] req :> S (Response SignIn)
 
-type S res = Post '[JSON] (H res) 
+type S res = Post '[ JSON] (H res)
 
-type H res = Headers '[ Header "Set-Cookie" SetCookie
-                      , Header "Access-Control-Allow-Origin" String
-                      , Header "Access-Control-Allow-Credentials" Bool ] res
+type H res
+   = Headers '[ Header "Set-Cookie" SetCookie
+              , Header "Access-Control-Allow-Origin" String
+              , Header "Access-Control-Allow-Headers" String
+              , Header "Access-Control-Allow-Credentials" Bool ] res
 
-data SignUp = SignUp 
-  { signUpUsername :: Text
-  , signUpPassword :: Text }
-  deriving stock (Eq, Ord, Show, Read, Generic)
+data SignUp =
+  SignUp
+    { signUpUsername :: Text
+    , signUpPassword :: Text
+    }
+  deriving  (Eq, Ord, Show, Read, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
-data instance Response SignUp = SignedUp
-  deriving stock (Eq, Ord, Show, Read, Generic)
+data instance  Response SignUp = SignedUp
+                                   deriving stock (Eq, Ord, Show, Read, Generic)
+                                   deriving anyclass (FromJSON, ToJSON)
+
+data SignIn =
+  SignIn
+    { signInUsername :: Text
+    , signInPassword :: Text
+    }
+  deriving  (Eq, Ord, Show, Read, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
-data SignIn = SignIn
-  { signInUsername :: Text
-  , signInPassword :: Text }
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+data instance  Response SignIn = SignedIn Text
+                                   deriving stock (Eq, Ord, Show, Read, Generic)
+                                   deriving anyclass (FromJSON, ToJSON)
 
-data instance Response SignIn = SignedIn Text
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
-
-newtype NewArticle = NewArticle
-  { newArticleName :: Text }
+newtype NewArticle =
+  NewArticle
+    { newArticleName :: Text
+    }
   deriving newtype (Eq, Ord, Show, Read, FromJSON, ToJSON)
 
-data instance Response NewArticle = ArticleCreated (Key Article)
-  deriving stock (Eq, Ord, Show, Read, Generic)
+data instance  Response NewArticle = ArticleCreated (Key Article)
+                                       deriving stock (Eq, Ord, Show, Read, Generic)
+                                       deriving anyclass (FromJSON, ToJSON)
+
+data NewDraft =
+  NewDraft
+    { newDraftArticle :: Key Article
+    , newDraftContents :: Text
+    }
+  deriving  (Eq, Ord, Show, Read, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
-data NewDraft = NewDraft
-  { newDraftArticle :: Key Article
-  , newDraftContents :: Text }
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+data instance  Response NewDraft = DraftCreated
+                                     deriving stock (Eq, Ord, Show, Read, Generic)
+                                     deriving anyclass (FromJSON, ToJSON)
 
-data instance Response NewDraft = DraftCreated
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
-
-newtype LatestDraft = LatestDraft { unLatestDraft :: Key Article }
+newtype LatestDraft =
+  LatestDraft
+    { unLatestDraft :: Key Article
+    }
   deriving newtype (Eq, Ord, Show, Read, FromJSON, ToJSON, FromHttpApiData)
 
-data instance Response LatestDraft = LatestDraftFound (Key Draft) (Key User) Text UTCTime | LatestDraftNotFound
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+data instance  Response LatestDraft = LatestDraftFound (Key Draft)
+                                                       (Key User) Text UTCTime
+                                    | LatestDraftNotFound
+                                        deriving stock (Eq, Ord, Show, Read, Generic)
+                                        deriving anyclass (FromJSON, ToJSON)
 
-newtype DraftComments = DraftComments { unDraftComments :: Key Draft }
+newtype DraftComments =
+  DraftComments
+    { unDraftComments :: Key Draft
+    }
   deriving newtype (Eq, Ord, Show, Read, FromJSON, ToJSON, FromHttpApiData)
 
-data instance Response DraftComments = DraftCommentsFound [Comment]
-  deriving stock (Eq, Ord, Show, Read, Generic)
+data instance 
+     Response DraftComments = DraftCommentsFound [Comment]
+                                deriving stock (Eq, Ord, Show, Read, Generic)
+                                deriving anyclass (FromJSON, ToJSON)
+
+data LinkDraft =
+  LinkDraft
+    { linkDraftFrom :: Key Draft
+    , linkDraftTag :: SomeTag
+    , linkDraftTo :: Key Void
+    }
+  deriving  (Eq, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
-data LinkDraft = LinkDraft
-  { linkDraftFrom :: Key Draft
-  , linkDraftTag :: SomeTag
-  , linkDraftTo :: Key Void 
-  } deriving stock (Eq, Generic)
-    deriving anyclass (FromJSON, ToJSON)
-
-data instance Response LinkDraft = Linked
-  deriving stock (Eq, Ord, Show, Read, Generic)
-  deriving anyclass (FromJSON, ToJSON)
+data instance  Response LinkDraft = Linked
+                                      deriving stock (Eq, Ord, Show, Read, Generic)
+                                      deriving anyclass (FromJSON, ToJSON)
--- geopolitik/src/Geopolitik/Server.hs
+++ geopolitik/src/Geopolitik/Server.hs
@@ -1,21 +1,21 @@
 module Geopolitik.Server where
 
-import Data.Text (Text)
-import Geopolitik.Database
-import Geopolitik.Ontology
-import Geopolitik.Auth
+import Control.Monad.Reader.Class
 import Control.Monad.Trans
-import Geopolitik.API
-import Servant
-import Servant.Server.Experimental.Auth
-import Data.Time.Clock
+import Data.Text (Text)
 import Data.Text.Encoding
+import Data.Time.Clock
 import Data.UUID
-import System.Random
-import qualified Network.Wai as Wai
 import Database.PostgreSQL.Simple hiding ((:.))
 import Database.PostgreSQL.Simple.SqlQQ
-import Control.Monad.Reader.Class
+import Geopolitik.API
+import Geopolitik.Auth
+import Geopolitik.Database
+import Geopolitik.Ontology
+import qualified Network.Wai as Wai
+import Servant
+import Servant.Server.Experimental.Auth
+import System.Random
 import Web.Cookie
 
 type M = DatabaseT Handler
@@ -27,106 +27,131 @@ ctxProxy :: Proxy Ctx
 ctxProxy = Proxy
 
 ctx :: Connection -> Context Ctx
-ctx conn = mkAuthHandler validate :. EmptyContext where
-  validate :: Wai.Request -> Handler User
-  validate req = do
-    cookie <- maybe (throwError err401 { errReasonPhrase = "You have no cookies" }) return 
-      $ lookup "cookie" (Wai.requestHeaders req)
-    token <- maybe (throwError err401 { errReasonPhrase = "You don't have a geopolitik-user cookie"}) return
-      $ lookup "geopolitik-user" $ parseCookies cookie
-    liftIO $ print token
-    maybe (throwError err401 { errReasonPhrase = "Could not validate token" }) return =<< runSharedDatabaseT conn (validateToken token) 
+ctx conn = mkAuthHandler validate :. EmptyContext
+  where
+    validate :: Wai.Request -> Handler User
+    validate req = do
+      cookie <-
+        maybe
+          (throwError err401 {errReasonPhrase = "You have no cookies"})
+          return $
+        lookup "cookie" (Wai.requestHeaders req)
+      token <-
+        maybe
+          (throwError
+             err401
+               {errReasonPhrase = "You don't have a geopolitik-user cookie"})
+          return $
+        lookup "geopolitik-user" $ parseCookies cookie
+      liftIO $ print token
+      maybe
+        (throwError err401 {errReasonPhrase = "Could not validate token"})
+        return =<<
+        runSharedDatabaseT conn (validateToken token)
 
 server :: ServerT GeopolitikAPI M
-server = account :<|> article :<|> serveDirectoryWebApp "frontend" 
+server = account :<|> article :<|> serveDirectoryWebApp "frontend"
 
 account :: ServerT AccountAPI M
 account = signup :<|> signin :<|> newToken
 
 signup :: SignUp -> M (Response SignUp)
-signup (SignUp username password) 
-  = do
-    lookupUsersByUsername [username] >>= \case
-      [] -> do
-        userCreationDate <- liftIO getCurrentTime
-        userID <- liftIO ((Key . toText) <$> randomIO)
-        insertUsers [User{..}] 
-        return SignedUp
-      _ -> throwError err403 { errReasonPhrase = "Username already exists" }
+signup (SignUp username password) = do
+  lookupUsersByUsername [username] >>= \case
+    [] -> do
+      userCreationDate <- liftIO getCurrentTime
+      userID <- liftIO ((Key . toText) <$> randomIO)
+      insertUsers [User {..}]
+      return SignedUp
+    _ -> throwError err403 {errReasonPhrase = "Username already exists"}
 
 signin :: SignIn -> M (H (Response SignIn))
-signin (SignIn username password)
-  = lookupUsersByUsername [username] >>= \case
-      [User{userID = sessionOwner, password = userPassword}] -> do
-        if (password == userPassword) then do
+signin (SignIn username password) =
+  lookupUsersByUsername [username] >>= \case
+    [User {userID = sessionOwner, password = userPassword}] -> do
+      if (password == userPassword)
+        then do
           sessionCreationDate <- liftIO getCurrentTime
-          sessionToken <- liftIO (toText <$> randomIO)      
-          sessionID <- liftIO ((Key . toText) <$> randomIO) 
-          let sess = Session{..}
+          sessionToken <- liftIO (toText <$> randomIO)
+          sessionID <- liftIO ((Key . toText) <$> randomIO)
+          let sess = Session {..}
           insertSessions [sess]
-          return $ addHeader (def 
-                   { setCookieName = "geopolitik-user"
-                   , setCookieSameSite = Just sameSiteStrict
-                   , setCookieMaxAge = Nothing
-                   , setCookiePath = Just "../../"
-                   , setCookieHttpOnly = True
-                   , setCookieValue = encodeUtf8 sessionToken }) 
-                 $ addHeader "http://localhost:8080"
-                 $ addHeader True
-                 $ SignedIn sessionToken
-        else throwError err403 { errReasonPhrase = "Wrong password" }
-      _ -> throwError err403 { errReasonPhrase = "Wrong username" }
+          return $
+            addHeader
+              (def
+                 { setCookieName = "geopolitik-user"
+                 , setCookieSameSite = Just sameSiteStrict
+                 , setCookieMaxAge = Nothing
+                 , setCookiePath = Just "/"
+                 , setCookieValue = encodeUtf8 sessionToken
+                 }) $
+            addHeader "http://localhost:8080" $
+            addHeader "Accept" $
+            addHeader True $ SignedIn sessionToken
+        else throwError err403 {errReasonPhrase = "Wrong password"}
+    _ -> throwError err403 {errReasonPhrase = "Wrong username"}
 
 newToken :: User -> M (H (Response SignIn))
-newToken User{..} = do
+newToken User {..} = do
   signin $ SignIn username password
 
 article :: ServerT ArticleAPI M
 article user = newArticle user :<|> draft user
 
 newArticle :: User -> NewArticle -> DatabaseT Handler (Response NewArticle)
-newArticle User{userID = articleOwner} (NewArticle articleName) = do
+newArticle User {userID = articleOwner} (NewArticle articleName) = do
   articleCreationDate <- liftIO getCurrentTime
-  articleID <- liftIO ((Key . toText) <$> randomIO) 
-  insertArticles [Article{..}]
+  articleID <- liftIO ((Key . toText) <$> randomIO)
+  insertArticles [Article {..}]
   return (ArticleCreated articleID)
 
 link :: User -> LinkDraft -> DatabaseT Handler (Response LinkDraft)
-link u@User{..} (LinkDraft linkDraft (SomeTag tag) linkEntity) = do
-  linkAuth u tag linkDraft (obvious tag linkEntity) 
+link u@User {..} (LinkDraft linkDraft (SomeTag tag) linkEntity) = do
+  linkAuth u tag linkDraft (obvious tag linkEntity)
   linkID <- liftIO ((Key . toText) <$> randomIO)
   let linkTag = SomeTag tag
-  insertLinks [Link{..}]
+  insertLinks [Link {..}]
   return Linked
 
 draft :: User -> ServerT DraftAPI M
-draft user = newDraft user :<|> comments user :<|> link user :<|> latest user :<|> latest' user
+draft user =
+  newDraft user :<|> comments user :<|> link user :<|> latest user :<|>
+  latest' user
 
 comments :: User -> DraftComments -> M (Response DraftComments)
 comments _ (DraftComments draftKey) = do
   c <- ask
-  fmap DraftCommentsFound $ liftIO $ query c [sql|
+  fmap DraftCommentsFound $
+    liftIO $
+    query
+      c
+      [sql|
      select (comments.id, comments.author, comments.content) from comments
         inner join links on comments.id = links.entity 
         where comments.id in ?
         and links.tag = 'CommentTag';
-    |] (Only (In [draftKey]))
+    |]
+      (Only (In [draftKey]))
 
 newDraft :: User -> NewDraft -> M (Response NewDraft)
-newDraft u@User{userID = draftAuthor} (NewDraft draftArticle draftContents) = do
+newDraft u@User {userID = draftAuthor} (NewDraft draftArticle draftContents) = do
   draftCreationDate <- liftIO getCurrentTime
   draftID <- liftIO ((Key . toText) <$> randomIO)
-  insertDraft u Draft{..}
+  insertDraft u Draft {..}
   return DraftCreated
 
 latest :: User -> LatestDraft -> M (Response LatestDraft)
-latest _ (LatestDraft articleKey) 
-  = latestDraft articleKey >>= \case
-      Just Draft{..} -> return $ LatestDraftFound draftID draftAuthor draftContents draftCreationDate
-      Nothing -> return LatestDraftNotFound
+latest _ (LatestDraft articleKey) =
+  latestDraft articleKey >>= \case
+    Just Draft {..} ->
+      return $
+      LatestDraftFound draftID draftAuthor draftContents draftCreationDate
+    Nothing -> return LatestDraftNotFound
 
 latest' :: User -> Text -> Text -> M (Response LatestDraft)
-latest' _ username articleName
-  = latestDraft' username articleName >>= \case
-      Just Draft{..} -> return $ LatestDraftFound draftID draftAuthor draftContents draftCreationDate
-      Nothing -> return LatestDraftNotFound
+latest' _ username articleName =
+  latestDraft' username articleName >>= \case
+    Just Draft {..} ->
+      return $
+      LatestDraftFound draftID draftAuthor draftContents draftCreationDate
+    Nothing -> return LatestDraftNotFound
