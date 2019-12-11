d1f8da528c48d411406a6c2ff9a0a1e23c7f6dbd
Samuel Schlesinger <samuel@simspace.com>
2019-12-11 02:52:12 -0500

    factored in some of the new auth code

--- geopolitik-backend/src/Geopolitik/Auth.hs
+++ geopolitik-backend/src/Geopolitik/Auth.hs
@@ -1,55 +1,18 @@
 module Geopolitik.Auth (linkAuth, draftAuth, collaboratorAuth) where
 
 import Control.Monad.Except
-import Data.Text (unpack)
 import Geopolitik.Database
 import Geopolitik.Monad
 import Geopolitik.Ontology
 import Servant
-
-exists :: MonadGeopolitik m => Tag a -> Key a -> m ()
-exists tag k = lookupEntities tag [k] >>= \case 
-  [_] -> return ()
-  [] -> throwError err404
-    { errReasonPhrase = "Entity does not exist" }
-  _ -> throwError err500
-    { errReasonPhrase = "Invalid database state: exists " <> show tag }
+import Geopolitik.Auth.Named
+import Geopolitik.Auth.CanLink
+import Geopolitik.Auth.IsOwner
 
 linkAuth :: MonadGeopolitik m => User -> Tag a -> Key Draft -> Key a -> m () 
-linkAuth User{..} tag draftKey entity 
-  = exists tag entity >> case tag of
-      ArticleTag -> draftAuthorCheck "Link Article" userID draftKey
-      UserTag -> draftAuthorCheck "Link User" userID draftKey 
-      CommentTag -> draftExistsCheck "Link Comment" draftKey
-      LocationTag -> draftAuthorCheck "Link Location" userID draftKey
-      DraftTag -> draftAuthorCheck "Link Draft" userID draftKey
+linkAuth user@User{..} tag draftKey entity 
+  = named user \u -> named draftKey \d -> named entity \e -> () <$ canLink u tag d e
 
-draftAuthorCheck 
-  :: MonadGeopolitik m 
-       => String 
-       -> Key User 
-       -> Key Draft 
-       -> m ()
-draftAuthorCheck reasonPrefix userID draftKey = lookupDrafts [draftKey] >>= \case
-  [ Draft{..} ] -> 
-    if draftAuthor == userID 
-      then return () 
-      else throwError err403 
-        { errReasonPhrase = reasonPrefix 
-                         <> ": You are not the author of draft with key " 
-                         <> unpack (getKey draftKey) }
-  _       -> throwError err404 
-        { errReasonPhrase = reasonPrefix 
-                         <> ": Could not find draft with key " 
-                         <> unpack (getKey draftKey) }
-
-draftExistsCheck :: MonadGeopolitik m => String -> Key Draft -> m ()
-draftExistsCheck reasonPrefix draftKey = lookupDrafts [draftKey] >>= \case
-  [ Draft{..} ] -> return ()
-  _ -> throwError err404 
-    { errReasonPhrase = reasonPrefix 
-                     <> ": Could not find draft with key " 
-                     <> unpack (getKey draftKey) }
 
 draftAuth :: MonadGeopolitik m => User -> Key Article -> m ()
 draftAuth User{..} article = lookupArticles [article] >>= \case
@@ -66,13 +29,4 @@ draftAuth User{..} article = lookupArticles [article] >>= \case
     { errReasonPhrase = "Invalid database state: draftAuth (B)" }
 
 collaboratorAuth :: MonadGeopolitik m => Key User -> Key Article -> m ()
-collaboratorAuth user article = lookupArticles [article] >>= \case
-  [ Article{..} ] -> 
-    if articleOwner == user 
-      then return ()
-      else throwError err403
-        { errReasonPhrase = "User is not the owner of this article" }
-  [] -> throwError err404
-    { errReasonPhrase = "Article does not exist" }
-  _ -> throwError err500
-    { errReasonPhrase = "Invalid database state: collaboratorAuth" } 
+collaboratorAuth user article = named user \u -> named article \a -> () <$ isOwner u a
